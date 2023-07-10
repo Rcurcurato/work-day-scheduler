47b7b94e4d8756c8b7836159bfda96f862c9b77b
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-07-10 14:33:44 -0400

    updated hours in index.html/ finalized script.js code/ updated and added video to README

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //fully loads dom before executing function
-$(document).ready(function () {
+$(window).on('load', function () {
 
   //add listener for click events to the save button
   //when save button is clicked
@@ -7,43 +7,45 @@ $(document).ready(function () {
   saveButton.on('click', function () {
     var timeBlockEl = $(this).parent().attr('id');
     var userInput = $(this).prev().val();
-    
+
     //then the data is saved to local storage
     localStorage.setItem(timeBlockEl, userInput);
   });
-});
 
-//determines whether it is past, present, or future
-var difTimes = $('.time-block');
-var currentHour = dayjs().hour();
+  //determines whether it is past, present, or future
+  var difTimes = $('.time-block');
 
-difTimes.each(function () {
-  var timeBlockEl = $(this).attr('id');
-  var hour = parseInt(timeBlockEl.split('-')[1]);
+  difTimes.each(function () {
+    var timeBlockEl = $(this).attr('id');
+    var hour = parseInt(timeBlockEl.split('-')[1]);
+    var currentHour = parseInt(dayjs().format('H'));
 
-  if (hour < currentHour) {
-    $(this).removeClass('past', 'present', 'future').addClass('past');
-  } else if (hour === currentHour) {
-    $(this).removeClass('past', 'present', 'future').addClass('present');
-  } else {
-    $(this).removeClass('past', 'present', 'future').addClass('future');
+    //console.log(hour)
+    //console.log(currentHour)
+
+    if (hour < currentHour) {
+      $(this).removeClass('past present future').addClass('past');
+    } else if (hour === currentHour) {
+      $(this).removeClass('past present future').addClass('present');
+    } else {
+      $(this).removeClass('past present future').addClass('future');
+    }
+  });
+
+  //date and time function displayed in the header of the page
+  function dateAndTime() {
+    var dateAndTime = $('#currentDay');
+    var currentDate = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    dateAndTime.text(currentDate);
   }
+  //calls function for date and time
+  dateAndTime();
+  setInterval(dateAndTime, 1000);
+
+  //retrieves data from local storage and saves it to the page 
+  difTimes.each(function () {
+    var timeBlockEl = $(this).attr('id');
+    var userInput = localStorage.getItem(timeBlockEl);
+    $(this).find('textarea').val(userInput);
+  });
 });
-
-//date and time function displayed in the header of the page
-function dateAndTime() {
-  var dateAndTime = $('#currentDay');
-  var currentDate = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  dateAndTime.text(currentDate);
-}
-//calls function for date and time
-dateAndTime();
-setInterval(dateAndTime, 1000);
-
-//retrieves data from local storage and saves it to the page 
-difTimes.each(function () {
-  var timeBlockEl = $(this).attr('id');
-  var userInput = localStorage.getItem(timeBlockEl);
-  $(this).find('textarea').val(userInput);
-});
-
